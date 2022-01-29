dc1f0696f521e660811177931eb848ce858a7715
ஈஷா முருகா <86954999+ishamuruga@users.noreply.github.com>
2022-01-29 14:54:18 +0530

    Delete Source/karuna-volunteering-portal directory

--- Source/karuna-volunteering-portal/projects/k-vol-services/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of k-vol-services
- */
-
-export * from './lib/k-vol-services.service';
-export * from './lib/k-vol-services.component';
-export * from './lib/k-vol-services.module';
