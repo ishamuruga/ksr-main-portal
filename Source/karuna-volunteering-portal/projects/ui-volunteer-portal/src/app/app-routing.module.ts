ff1b3f2be6bf208b78b68d98106452a288c64045
bashabhai <super@gmail.com>
2022-02-13 17:59:46 +0530

    new login

--- Source/karuna-volunteering-portal/projects/ui-volunteer-portal/src/app/app-routing.module.ts
+++ Source/karuna-volunteering-portal/projects/ui-volunteer-portal/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { LandingComponent } from './secured/landing/landing.component';
+import { NewVolunteerRegistrationComponent } from './components/open/new-volunteer-registration/new-volunteer-registration.component';
 
 const routes: Routes = [
   {path:'login',component:LoginComponent},
   {path:'dashboard',component:LandingComponent},
+  {path:'vol-registration',component : NewVolunteerRegistrationComponent},
   {path:'',redirectTo:'/login',pathMatch:'full'}
 ];
 
